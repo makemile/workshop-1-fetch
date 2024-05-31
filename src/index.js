b2e2572f11ebaf40f80ca9fc46934cf4172e8659
Kengya <mkengya@gmail.com>
2024-05-31 18:58:54 -0500

    add styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 import { URL_BASE } from "./Api/Apis";
 
+const appNode = document.querySelector('#app')
+const formatPrice = (price) =>   {
+ const newPrice = new window.Intl.NumberFormat('en-En', {
+  style: "currency",
+  currency: "USD"
+ }).format(price);
+ return newPrice;
+}
 async function getAvo() {
   try {
     const render = [];
@@ -11,16 +15,27 @@ async function getAvo() {
     const data = await response.json();
     data.data.forEach((avo, index) => {
       const container = document.createElement("article");
+      const btnAddToCar = document.createElement("button");
+      btnAddToCar.textContent = "Add to cart"
+      btnAddToCar.id = avo.id;
+      btnAddToCar.className = "bg-white hover:bg-cyan-600 rounded w-24 p-2 font-normal cursor-pointer transition duration-700 ease-in-out"
+      container.classList.add("card");
       const img = document.createElement("img");
       img.src = URL_BASE + avo.image;
+      img.alt = avo.name;
+      img.classList.add("card__image")
       const title = document.createElement("h3");
       title.textContent = avo.name;
+      title.classList.add("card__title");
+      // title.className = "text-2xl";
       const price = document.createElement("span");
-      price.textContent = avo.price;
-      container.append(img, title, price);
+      price.textContent = formatPrice(avo.price);
+      price.classList.add("card__price");
+      // price.className = "text-2xl";
+      container.append(img, title, price, btnAddToCar);
       render.push(container);
     });
-    document.body.append(...render);
+    appNode.append(...render);
     console.log(data.data, "data");
   } catch (error) {
     console.error(error);
